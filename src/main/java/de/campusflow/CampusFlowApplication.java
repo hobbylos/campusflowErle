9668ae06cf82a732197f6efe0fc935145aeb003b
hobbylos <elel2005@gmx.de>
2026-09-18 14:52:32 +0200

    Grundstruktur eingerichtet

---
+++ src/main/java/de/campusflow/CampusFlowApplication.java
@@ -0,0 +1,13 @@
+package de.campusflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusFlowApplication.class, args);
+    }
+
+}
